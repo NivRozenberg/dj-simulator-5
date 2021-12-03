db2f5a9624f9089ddf6c30d407407bad85b1f804
NivRozenberg <niv.ro28@gmail.com>
2021-12-03 17:29:50 +0200

    deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// app.use(express.static(__dirname + '/dist/music-app'));
+// app.get('/*', function(req,res) {
+// res.sendFile(path.join(__dirname+
+// '/dist/music-app/index.html'));});
+// app.listen(process.env.PORT || 8080);
+
+
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/music-app'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/music-app/index.html'));});
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/music-app'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/music-app/'}),
+);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
